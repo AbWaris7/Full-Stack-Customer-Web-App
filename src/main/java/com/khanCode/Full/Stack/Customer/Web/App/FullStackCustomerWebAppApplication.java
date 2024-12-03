269bddba783a2321e247ddb01713147ee5d78b26
abdulwariskhan <hellokhan.ch@gmail.com>
2024-12-03 13:37:57 +0100

    Initial commit

---
+++ src/main/java/com/khanCode/Full/Stack/Customer/Web/App/FullStackCustomerWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.khanCode.Full.Stack.Customer.Web.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackCustomerWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackCustomerWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khanCode/Full/Stack/Customer/Web/App/FullStackCustomerWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khanCode.Full.Stack.Customer.Web.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackCustomerWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
