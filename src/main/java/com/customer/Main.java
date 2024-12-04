eb05798e6134203f2b7d5221b9a58e8214d3b680
abdulwariskhan <hellokhan.ch@gmail.com>
2024-12-04 04:22:21 +0100

    API Endpoint

--- src/main/java/com/customer/Main.java
+++ src/main/java/com/customer/Main.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -84,5 +85,11 @@ public class Main {
                    ", age=" + age +
                    '}';
        }
+   }
+
+   @GetMapping("/customers")
+    public List<Customer> getCustomers() {
+
+         return customers;
    }
 }
