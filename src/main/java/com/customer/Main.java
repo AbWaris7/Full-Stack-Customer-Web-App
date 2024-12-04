a3e8ab408e033db8aff7d2aa49db3f43d6992cc6
abdulwariskhan <hellokhan.ch@gmail.com>
2024-12-04 04:19:31 +0100

    fake database

--- src/main/java/com/customer/Main.java
+++ src/main/java/com/customer/Main.java
@@ -6,25 +6,38 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @SpringBootApplication
 @RestController
 @RequestMapping("/api")
 public class Main {
+    private static ArrayList<Customer> customers;
+     static {
+
+        customers = new ArrayList<>();
+        customers.add(new Customer(1,"Alex","alex@gamil.com",20));
+        customers.add(new Customer(2,"Bob","bob@gamil.com",30));
+        customers.add(new Customer(3,"Charlie","charlie@gamil.com",40));
+    }
 
     public static void main(String[] args) {
+        System.out.println(customers);
         SpringApplication.run(Main.class, args);
     }
 
 
 
-   class Customer {
+   static class Customer {
         private Integer id;
         private String name;
         private String email;
-        public Customer(Integer id, String name, String email) {
+        private Integer age;
+        public Customer(Integer id, String name, String email, Integer age ) {
             this.id = id;
             this.name = name;
             this.email = email;
+            this.age = age;
         }
        Customer() {
 
@@ -54,12 +67,21 @@ public class Main {
            this.email = email;
        }
 
+       public Integer getAge() {
+           return age;
+       }
+
+       public void setAge(Integer age) {
+           this.age = age;
+       }
+
        @Override
        public String toString() {
            return "Customer{" +
                    "id=" + id +
                    ", name='" + name + '\'' +
                    ", email='" + email + '\'' +
+                   ", age=" + age +
                    '}';
        }
    }
