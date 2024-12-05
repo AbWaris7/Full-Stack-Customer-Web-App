bc4d4be8d9f7e23cb8f0cff2c6719ff2afbed2ea
abdulwariskhan <hellokhan.ch@gmail.com>
2024-12-06 00:23:17 +0100

    added CustomerService class

---
+++ src/main/java/com/customer/customer/CustomerService.java
@@ -0,0 +1,21 @@
+package com.customer.customer;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerService {
+
+    private CustomerDao customerDao;
+    public CustomerService(CustomerDao customerDao) {
+        this.customerDao = customerDao;
+    }
+    public List<Customer> getAllCustomers() {
+        return customerDao.selectAllCustomer();
+    }
+
+    public Customer getCustomerById(int id) {
+        return customerDao.selectCustomerById(id);
+    }
+}
