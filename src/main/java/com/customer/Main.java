e23563b489724a5c24aba0babfae4b52ee4d4e53
abdulwariskhan <hellokhan.ch@gmail.com>
2024-12-06 00:23:00 +0100

    code main Class

--- src/main/java/com/customer/Main.java
+++ src/main/java/com/customer/Main.java
@@ -1,4 +1,5 @@
 package com.customer;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,91 +11,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
-@RestController
-@RequestMapping("/api")
 public class Main {
-    private static final ArrayList<Customer> customers;
-     static {
 
-        customers = new ArrayList<>();
-        customers.add(new Customer(1,"Alex","alex@gamil.com",20));
-        customers.add(new Customer(2,"Bob","bob@gamil.com",30));
-        customers.add(new Customer(3,"Charlie","charlie@gamil.com",40));
-    }
 
     public static void main(String[] args) {
-        System.out.println(customers);
         SpringApplication.run(Main.class, args);
     }
 
-
-
-   static class Customer {
-        private Integer id;
-        private String name;
-        private String email;
-        private Integer age;
-        public Customer(Integer id, String name, String email, Integer age ) {
-            this.id = id;
-            this.name = name;
-            this.email = email;
-            this.age = age;
-        }
-       Customer() {
-
-       }
-
-       public Integer getId() {
-           return id;
-       }
-
-       public void setId(Integer id) {
-           this.id = id;
-       }
-
-       public String getName() {
-           return name;
-       }
-
-       public void setName(String name) {
-           this.name = name;
-       }
-
-       public String getEmail() {
-           return email;
-       }
-
-       public void setEmail(String email) {
-           this.email = email;
-       }
-
-       public Integer getAge() {
-           return age;
-       }
-
-       public void setAge(Integer age) {
-           this.age = age;
-       }
-
-       @Override
-       public String toString() {
-           return "Customer{" +
-                   "id=" + id +
-                   ", name='" + name + '\'' +
-                   ", email='" + email + '\'' +
-                   ", age=" + age +
-                   '}';
-       }
-   }
-
-   @GetMapping("/customers")
-    private List<Customer> getCustomers() {
-
-         return customers;
-   }
-
-   @GetMapping("/customers/{id}")
-    private Customer getCustomer(@PathVariable Integer id) {
-         return customers.stream().filter(c -> c.getId().equals(id)).findFirst().orElseThrow(()->new IllegalArgumentException("Customer not found "+id));
-   }
 }
