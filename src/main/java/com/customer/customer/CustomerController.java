7d534fc0d0c25803aeda0adb6ecdcbf9f5d9df44
abdulwariskhan <hellokhan.ch@gmail.com>
2024-12-06 08:25:05 +0100

    changed class name

--- src/main/java/com/customer/customer/CustomerController.java
+++ src/main/java/com/customer/customer/CustomerController.java
@@ -3,19 +3,18 @@ package com.customer.customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-public class Controller {
+public class CustomerController {
 
 
     CustomerService customerService;
 
     @Autowired
-    public Controller(CustomerService customerService) {
+    public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
     }
 
