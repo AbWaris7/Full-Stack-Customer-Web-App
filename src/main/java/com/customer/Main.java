cde27ad0b187379d94339cd7f4f2145ea1c142bb
abdulwariskhan <hellokhan.ch@gmail.com>
2024-12-20 22:34:45 +0100

    added demo customer for testing purposes

--- src/main/java/com/customer/Main.java
+++ src/main/java/com/customer/Main.java
@@ -1,6 +1,13 @@
 package com.customer;
+import com.customer.customer.Customer;
+import com.customer.customer.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
 
 @SpringBootApplication
 public class Main {
@@ -8,6 +15,23 @@ public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+    @Bean
+    CommandLineRunner runner(CustomerRepository customerRepository) {
+        return args -> {
+            addCustomer(customerRepository);
+        };
+    }
+
+    private void addCustomer(CustomerRepository customerRepository) {
+
+
+
+        Customer alex = new Customer( "Alex", "alex@gamil.com", 20);
+        Customer bob = new Customer( "Bob", "bob@gamil.com", 30);
+        Customer abdul = new Customer( "Abdul", "abdul@gamil.com", 40);
+        List<Customer> customers = List.of(alex, bob, abdul);
+        customerRepository.saveAll(customers);
     }
 
 }
