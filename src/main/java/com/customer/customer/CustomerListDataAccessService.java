13e17231edb7dbb2e4f4537c500f5303009fade2
abdulwariskhan <hellokhan.ch@gmail.com>
2024-12-20 22:34:03 +0100

    added CustomerListDataAccessService

--- src/main/java/com/customer/customer/CustomerListDataAccessService.java
+++ src/main/java/com/customer/customer/CustomerListDataAccessService.java
@@ -5,17 +5,14 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.List;
 
-@Repository
-public class CustomerDataAccessService implements CustomerDao {
+@Repository("list")
+public class CustomerListDataAccessService implements CustomerDao {
 
     private static final ArrayList<Customer> customers;
 
     static {
 
         customers = new ArrayList<>();
-        customers.add(new Customer(1, "Alex", "alex@gamil.com", 20));
-        customers.add(new Customer(2, "Bob", "bob@gamil.com", 30));
-        customers.add(new Customer(3, "Charlie", "charlie@gamil.com", 40));
     }
 
     @Override
