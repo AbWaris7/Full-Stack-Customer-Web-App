14fe5c7e0fe333b7ad3e1d7dd186de261c4c8a7f
abdulwariskhan <hellokhan.ch@gmail.com>
2024-12-20 22:33:53 +0100

    added CustomerJPADataAccessService.java

---
+++ src/main/java/com/customer/customer/CustomerJPADataAccessService.java
@@ -0,0 +1,25 @@
+package com.customer.customer;
+
+import jakarta.persistence.EntityManager;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository("jpa")
+public class CustomerJPADataAccessService implements CustomerDao{
+
+    private final CustomerRepository customerRepository;
+    public CustomerJPADataAccessService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+    @Override
+    public List<Customer> selectAllCustomer() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Customer selectCustomerById(int id) {
+        return customerRepository.findById(id).orElse(null);
+    }
+}
